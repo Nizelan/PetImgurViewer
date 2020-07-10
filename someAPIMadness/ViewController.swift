dadb1376203ccc53f95ea2b39c3553bab61c1c33
Nizelan <saintnizelan@gmail.com>
2020-07-10 16:33:20 +0300

    Initial Commit

---
+++ someAPIMadness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  someAPIMadness
+//
+//  Created by Nizelan on 10.07.2020.
+//  Copyright © 2020 Nizelan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
