668b016bb7843e5c058e3935d084173289e86cde
Nizelan <saintnizelan@gmail.com>
2020-07-22 14:57:49 +0300

    authorization and fetching work now.Almost. Commit

--- Models/NetworkManager.swift
+++ Models/NetworkManager.swift
@@ -9,28 +9,50 @@
 import Foundation
 
 struct NetworkManadger {
+    //Authorization
     
-    func fetchGallery() {
-        let urlString = "https://api.imgur.com/3/gallery/top/top/week/17?showViral=true&mature=true&album_previews=true"
-        
+    func authorization(url: URL) {
         let httpHeaders = ["Authorization": "Client-ID 094e934ce523296"]
-        
-        guard let url = URL(string: urlString) else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = httpHeaders
         
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
+    
+    //Fetch data
+    
+    func fetchGallery() {
+        let urlString = "https://api.imgur.com/3/gallery/top/top/week/17?showViral=true&mature=true&album_previews=true"
+      
+        guard let url = URL(string: urlString) else { return }
+       
+        authorization(url: url)
+        
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                let dataString = String(data: data, encoding: .utf8)
-                print(dataString!)
                 self.parseJSON(withData: data)
             }
         }
         task.resume()
     }
+    
+    //Parse JSON
     
     func parseJSON(withData data: Data) {
         let decoder = JSONDecoder()
