f3abbdf0e59d7fca3a3a7968ffbd72d260728065
Nizelan <saintnizelan@gmail.com>
2020-07-22 19:27:18 +0300

    hope its good commit

--- Models/NetworkManager.swift
+++ Models/NetworkManager.swift
@@ -9,47 +9,35 @@
 import Foundation
 
 struct NetworkManadger {
-    //Authorization
     
-    func authorization(url: URL) {
+    //Fetch data
+    
+    func fetchGallery() {
+        let urlString = "https://api.imgur.com/3/gallery/top/top/week/17?showViral=true&mature=true&album_previews=true"
+        
         let httpHeaders = ["Authorization": "Client-ID 094e934ce523296"]
+        
+        guard let url = URL(string: urlString) else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = httpHeaders
         
         URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print(response)
-            }
-            
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+           if let response = response {
+               print(response)
+           }
+           
+           if let data = data {
+               do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
                     print(json)
-                } catch {
+                    self.parseJSON(withData: data)
+               } catch {
                     print(error)
-                }
-            }
+               }
+           }
         }.resume()
-    }
-    
-    //Fetch data
-    
-    func fetchGallery() {
-        let urlString = "https://api.imgur.com/3/gallery/top/top/week/17?showViral=true&mature=true&album_previews=true"
-      
-        guard let url = URL(string: urlString) else { return }
-       
-        authorization(url: url)
-        
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: url) { data, response, error in
-            if let data = data {
-                self.parseJSON(withData: data)
-            }
-        }
-        task.resume()
     }
     
     //Parse JSON
