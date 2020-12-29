0504cf3bb01ef43adb3355fde03b849e3abf6637
Nizelan <saintnizelan@gmail.com>
2020-12-29 17:27:51 +0300

    work with player
    add volume, video-progress bars and play faster and play backward buttons

--- Controllers/VideoViewController.swift
+++ Controllers/VideoViewController.swift
@@ -14,39 +14,63 @@ class VideoViewController: UIViewController {
 
     var name: String?
     var link: String?
+    var isFirst = true
 
     @IBOutlet weak var titleLable: UILabel!
     @IBOutlet weak var videoPlayer: CustomVideoPlayer!
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var videoProgresSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         videoPlayer.videoLink = link
         setupTitle(title: name)
-    }
-
-    func playVideo() {
-        guard let imageLink = link else { return }
-        if imageLink.contains("mp4") {
-            let url = URL(string: imageLink)
-            let player = AVPlayer(url: url!)
-            let vc = AVPlayerViewController()
-            vc.player = player
-
-            present(vc, animated: true) {
-                vc.player?.play()
-            }
-        }
+        videoPlayer.player?.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 10000),
+                                                    queue: .main, using: { (time) in
+                                                        self.videoProgresSlider.value = Float(time.seconds)
+        })
     }
 
     @IBAction func playButtonAction(_ sender: Any) {
+        if videoProgresSlider.value == videoProgresSlider.maximumValue {
+            videoPlayer.player?.seek(to: CMTime(seconds: 0, preferredTimescale: 1000))
+        }
+
         if videoPlayer.player!.timeControlStatus == .playing {
             playButton.setImage(UIImage(named: "PlayButton"), for: .normal)
             videoPlayer.pause()
         } else {
             playButton.setImage(UIImage(named: "PauseButton"), for: .normal)
             videoPlayer.play()
+            if isFirst {
+                setupProgressVideoSlider()
+            }
         }
+    }
+
+    @IBAction func playFaster(_ sender: Any) {
+        if videoPlayer.player?.currentItem?.currentTime() == videoPlayer.player?.currentItem?.duration {
+            videoPlayer.player?.currentItem?.seek(to: .zero)
+        }
+        videoPlayer.player?.rate = min(videoPlayer.player!.rate + 2.0, 2.0)
+    }
+
+    @IBAction func playBackward(_ sender: Any) {
+        if videoPlayer.player?.currentItem?.currentTime() == .zero {
+            if let itemDuration = videoPlayer.player?.currentItem?.duration {
+                videoPlayer.player?.currentItem?.seek(to: itemDuration)
+            }
+        }
+        videoPlayer.player?.rate = max(videoPlayer.player!.rate - 2.0, -2.0)
+    }
+
+    @IBAction func volumeAction(_ sender: UISlider) {
+        videoPlayer.player?.volume = volumeSlider.value
+    }
+
+    @IBAction func videoProgresSliderAction(_ sender: Any) {
+        videoPlayer.player?.seek(to: CMTime(seconds: Double(videoProgresSlider.value), preferredTimescale: 1000))
     }
 
     func setupTitle(title: String?) {
@@ -55,5 +79,16 @@ class VideoViewController: UIViewController {
         } else {
             titleLable.text = name
         }
+    }
+}
+
+extension VideoViewController {
+
+    func setupProgressVideoSlider() {
+        guard let duration = self.videoPlayer.player?.currentItem?.asset.duration.seconds else {
+            print("\(Self.self) error: now have Current Item duration time")
+            return
+        }
+        self.videoProgresSlider.maximumValue = Float(duration)
     }
 }
