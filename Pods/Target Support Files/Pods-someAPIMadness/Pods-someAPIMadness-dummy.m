46c2dd92d5c54497bd66bed963c480ada671a70b
Nizelan <saintnizelan@gmail.com>
2021-04-13 12:09:30 +0300

    Rename and clean project

--- Pods/Target Support Files/Pods-someAPIMadness/Pods-someAPIMadness-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_someAPIMadness : NSObject
-@end
-@implementation PodsDummy_Pods_someAPIMadness
-@end
--- Pods/Target Support Files/Pods-someAPIMadnessTests/Pods-someAPIMadnessTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_someAPIMadnessTests : NSObject
-@end
-@implementation PodsDummy_Pods_someAPIMadnessTests
-@end
